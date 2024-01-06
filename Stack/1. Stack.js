a1939ea32a479be201a81e944662c0d92c097969
mamed-gasimov <mamed.gasymov@gmail.com>
2024-01-06 19:40:44 +0400

    stack methods

--- Stack/1. Stack.js
+++ Stack/1. Stack.js
@@ -11,7 +11,30 @@ class Stack {
     this.top = newNode;
     this.length = 1;
   }
+
+  // method name is push, but basically it is like unshift method, adds item on front, in this case on top
+  push(value) {
+    const newNode = new Node(value);
+    newNode.next = this.top;
+    this.top = newNode;
+    this.length++;
+    return this;
+  }
+
+  // pops item from the top of stack
+  pop() {
+    if (!this.top) return;
+
+    const temp = this.top;
+    this.top = this.top.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
 }
 
 const myStack = new Stack(1);
+myStack.push(2);
+myStack.push(3);
+myStack.push(4);
 console.log(myStack);
