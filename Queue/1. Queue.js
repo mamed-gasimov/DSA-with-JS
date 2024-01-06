529f2a6cef54da85e1e9de94d68bed181c7db67b
mamed-gasimov <mamed.gasymov@gmail.com>
2024-01-06 20:54:18 +0400

    queue

---
+++ Queue/1. Queue.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.first = newNode;
+    this.last = newNode;
+    this.length = 1;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (!this.first) return;
+
+    const temp = this.first;
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+      temp.next = null;
+    }
+
+    this.length--;
+    return temp;
+  }
+}
+
+const myQueue = new Queue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+myQueue.enqueue(4);
+console.log(myQueue);
