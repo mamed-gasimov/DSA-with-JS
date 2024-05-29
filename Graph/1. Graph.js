07b7b223dd7ffc368517b1eef19472c231e2bab3
mamed-gasimov <mamed.gasymov@gmail.com>
2024-05-29 11:56:50 +0400

    graphs - remove vertex

--- Graph/1. Graph.js
+++ Graph/1. Graph.js
@@ -24,16 +24,58 @@ class Graph {
     }
     return false;
   }
+
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) return;
+
+    const vertexArray = this.adjacencyList[vertex];
+    while (vertexArray.length) {
+      const poppedItem = vertexArray.pop();
+      if (this.adjacencyList[poppedItem]) {
+        this.adjacencyList[poppedItem] = this.adjacencyList[poppedItem].filter(
+          (v) => v !== vertex
+        );
+      }
+    }
+
+    delete this.adjacencyList[vertex];
+    return this;
+  }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (v) => v !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1
+      );
+      return true;
+    }
+    return false;
+  }
 }
 
 const graph = new Graph();
 graph.addVertex("A");
 graph.addVertex("B");
 graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
 
 graph.addEdge("A", "B");
 graph.addEdge("B", "C");
 graph.addEdge("A", "C");
-graph.addEdge("A", "C");
+graph.addEdge("A", "D");
+graph.addEdge("A", "E");
 
+console.log("Before: ");
+console.log(graph.adjacencyList);
+
+graph.removeEdge("A", "E");
+console.log("After removing edge: ");
+console.log(graph.adjacencyList);
+
+graph.removeVertex("C");
+console.log("After removing vertex: ");
 console.log(graph.adjacencyList);
