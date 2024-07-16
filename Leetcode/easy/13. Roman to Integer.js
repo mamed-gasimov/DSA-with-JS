ebfbc2f4220f94a6ef80c106b3077d9737ba4cc4
mamed-gasimov <mamed.gasymov@gmail.com>
2024-07-16 07:46:46 +0400

    roman to integer

---
+++ Leetcode/easy/13. Roman to Integer.js
@@ -0,0 +1,50 @@
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+//  Symbol       Value
+//  I             1
+//  V             5
+//  X             10
+//  L             50
+//  C             100
+//  D             500
+//  M             1000
+
+// Roman numerals are usually written largest to smallest from left to right.
+// However, the numeral for four is not IIII. Instead, the number four is written as IV.
+// Because the one is before the five we subtract it making four.
+// The same principle applies to the number nine, which is written as IX.
+// There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9.
+// X can be placed before L (50) and C (100) to make 40 and 90.
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+
+const romanToInt = function (s) {
+  const romanNums = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let current = romanNums[s[i]];
+    let next = romanNums[s[i + 1]];
+
+    if (i < s.length - 1 && current < next) {
+      result -= current;
+    } else {
+      result += current;
+    }
+  }
+
+  return result;
+};
+
+console.log(romanToInt("III")); // 3
+console.log(romanToInt("LVIII")); // 58
+console.log(romanToInt("MCMXCIV")); // 1994
